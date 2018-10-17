b61e4d2764ecf3be5d3fc4d7df9516d186948e3a
Gouda <gouda.clark@gmail.com>
2018-10-17 16:14:54 -0400

    Integers Skills

---
+++ ninja_skills.rb
@@ -0,0 +1,19 @@
+Booleans
+[1] pry(main)> a = true
+=> true
+[2] pry(main)> b = false
+=> false
+[3] pry(main)> a = b
+=> false
+
+[1] pry(main)> a = true
+=> true
+[2] pry(main)> b = false
+=> false
+[3] pry(main)> a != b
+=> true
+
+Integers
+1. 4
+2. 2.5
+3. 2.5
