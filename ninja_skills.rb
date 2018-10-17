a7c6c56fd2311c75756b4fb4bc0715c480a757fe
Gouda <gouda.clark@gmail.com>
2018-10-17 16:22:14 -0400

    String skills

--- ninja_skills.rb
+++ ninja_skills.rb
@@ -17,3 +17,22 @@ Integers
 1. 4
 2. 2.5
 3. 2.5
+
+STRINGS
+=> "Hi, I'm Christine"
+[7] pry(main)> likesTo = "long walks on the beach, kinda"
+=> "long walks on the beach, kinda"
+13] pry(main)> christine + " and I like " + likesTo
+=> "Hi, I'm Christine and I like long walks on the beach, kinda"
+[8] pry(main)> g = "2"
+=> "2"
+[9] pry(main)> gToNumber = 2
+=> 2
+[10] pry(main)> g + gToNumber
+TypeError: no implicit conversion of Integer into String
+from (pry):10:in `+'`
+[11] pry(main)> g.to_i
+=> 2
+
+INPUT AND OUTPUT
+
